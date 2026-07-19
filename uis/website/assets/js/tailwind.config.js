7c04f53b69cbf1ca8ee64549b094a40b54740dbb
ilfredo <luisalfredo612@gmail.com>
2026-07-19 15:19:41 +0000

    Landing y formulario completos

---
+++ uis/website/assets/js/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "ui-sans-serif", "system-ui", "sans-serif"]
+      },
+      colors: {
+        brand: {
+          50: "#f5f8ff",
+          100: "#e8efff",
+          300: "#98b2ff",
+          500: "#3458d6",
+          700: "#1f378f",
+          900: "#0f1b45"
+        }
+      }
+    }
+  }
+};
